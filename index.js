348143902e5547bb05a55a1047ebc227135be362
Thibault Person <tperson@siradel.com>
2018-05-31 14:11:06 +0200

    Add release

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const slash_token = process.env.SLACK_SLASH_TOKEN || 'test';
 const dstDir = process.env.OPENSHIFT_DATA_DIR || '.';
 
 const shoutFile = path.join(dstDir, 'shout.data');
+const releaseFile = path.join(dstDir, 'release.data');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 let lastMessage = undefined;
+let releaseIP = undefined;
 
 
 app.post('/shout', (req, res) => {
@@ -62,6 +64,65 @@ app.get('/shout', (req, res) => {
 		}
 		lastMessage = data;
 		res.send(lastMessage || '');
+	});
+});
+
+
+app.post('/release', (req, res) => {
+    if (!req || !req.body) {
+		res.status(403);
+		return;
+	}
+
+	// check token
+	if (req.body.token !== slash_token)
+	{
+		res.status(403);
+		res.send({ error: 'token not valid' });
+		return;
+    }
+    
+    // get command
+	if (!req.body.text || req.body.text !== 'begin' || req.body.text !== 'end') {
+		res.status(403);
+		res.send({ error: 'Option is not valid.' });
+		return;
+	}
+    
+	releaseIP = req.body.text === 'begin' ? '1' : '0';
+	fs.writeFile(releaseFile, releaseIP, (err) => {
+		if (err) {
+			console.error('Failed to store shouted message:', err);
+		}
+		let text = '';
+		if (req.body.text === 'begin') {
+			text = 'Le dashboard va passer en mode release.'
+		}
+		if (req.body.text === 'end') {
+			text = 'Le dashboard va quitter le mode release.'
+		}
+
+
+		res.send({
+			response_type: 'ephemeral',
+			text: `text`
+		});
+	});
+});
+
+
+
+app.get('/release', (req, res) => {
+	if (releaseIP !== undefined) {
+		return releaseIP;
+	}
+
+	fs.readFile(releaseFile, (err, data) => {
+		if (err) {
+			console.error('Failed to read file:', err);
+		}
+		releaseIP = data;
+		res.send(releaseIP || '0');
 	});
 });
 
